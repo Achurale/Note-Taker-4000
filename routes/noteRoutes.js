2e7627419cad3f9bdd43485952f41843863f60af
Roymand <chuyirlee@live.com>
2023-09-20 14:53:54 -0500

    Stored notes can now be viewed and json file is formatted nicely

--- routes/noteRoutes.js
+++ routes/noteRoutes.js
@@ -8,13 +8,7 @@ const router = express.Router();
 // Database pathing
 const notesPath = path.join(__dirname, '../db/db.json');
 
-// HTML routes
-router.get("/notes", (req, res) => {
-    res.sendFile(path.join(__dirname, '../public/notes.html'))
-})
-router.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, '../public/index.html'))
-})
+
 
 // API routes
 router.get('/api/notes', (req, res) => {
@@ -24,6 +18,7 @@ router.get('/api/notes', (req, res) => {
     })
 })
 
+// Post
 router.post('/api/notes', (req, res) => {
     const newNote = {
         title: req.body.title,
@@ -32,13 +27,26 @@ router.post('/api/notes', (req, res) => {
     }
 
     fs.readFile(notesPath, 'utf8', (err, data) => {
-        const notes = JSON.parse(data);
+
+        let notes = JSON.parse(data) || [];
+
         notes.push(newNote);
 
-        fs.writeFile(notesPath, JSON.stringify(notes), (err) => {
-            res.json(newNote)
-        })
-    })
+        fs.writeFile(notesPath, JSON.stringify(notes, null, 2), (err) => {
+
+            res.json(newNote);
+        });
+    });
+});
+
+// HTML routes
+router.get("/notes", (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/notes.html'))
 })
+router.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/index.html'))
+})
+
+// Bonus TO DO: Deletion
 
 module.exports = router;
